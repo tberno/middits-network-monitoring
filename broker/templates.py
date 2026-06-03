fdf33efb6e0aa5afd969fee081cd2d8d89dd2032
Berno <tberno@middlebury.edu>
2026-06-03 11:11:16 -0400

    fix: resolve merge conflict in templates.py

--- broker/templates.py
+++ broker/templates.py
@@ -5,4 +5,8 @@ ALERT_TEMPLATE = """{header}
 
 *Device:* {device}   *Severity:* {severity}{ip_line}{rule_line}{fired_line}{resolved_line}{downtime_line}{details_block}{link_line}"""
 
+<<<<<<< Updated upstream
 DETAILS_TEMPLATE = "\n\n*Details:*\n\x60\x60\x60{details}\x60\x60\x60"
+=======
+DETAILS_TEMPLATE = "\n\n*Details:*\n\x60\x60\x60{details}\x60\x60\x60"
+>>>>>>> Stashed changes
